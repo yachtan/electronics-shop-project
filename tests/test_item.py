9f0d8e62c540a7018883c793b57616f4c1fee46b
yachanova_tania <tan9l_85@mail.ru>
2023-09-16 22:34:50 +0300

    homework_3

--- tests/test_item.py
+++ tests/test_item.py
@@ -14,9 +14,28 @@ def test_item_init(phone):
     assert phone.price == 100.0
     assert phone.quantity == 20
 
+
+@pytest.fixture
+def phone_1():
+    return Item('samsung_0000', 100.0, 2)
+
+
+def test_item_init(phone_1):
+    """Проверяем создание класса с длинным именем"""
+    assert phone_1.name == 'samsung_00'
+
+def test_repr(phone):
+    assert repr(phone) == "Item('samsung', 100.0, 20)"
+
+
+def test_str(phone):
+    assert str(phone) == 'samsung'
+
+
 def test_item_calculate_total_price(phone):
     """Задавая цену и количество, ожидаем получить общую стоимость товара"""
     assert phone.calculate_total_price() == 2000.0
+
 
 def test_item_apply_discount(phone):
     """Задав скидочный коэффициент 0.8 и применив метод apply_discount,
@@ -25,6 +44,7 @@ def test_item_apply_discount(phone):
     phone.apply_discount()
     assert phone.price == 80.0
 
+
 def test_name(phone):
     """Проверяем возможность изменения наименования товара.
     При слишком длинном имени остается первые 10 символов"""
@@ -32,6 +52,7 @@ def test_name(phone):
     assert phone.name == 'Nokia 3310'
     phone.name = 'Nokia 2210123'
     assert phone.name == 'Nokia 2210'
+
 
 def test_string_to_number(phone):
     """Проверяем работу статического метода"""
@@ -42,9 +63,11 @@ def test_string_to_number(phone):
 
 @pytest.fixture
 def items_from_csv():
-    test_file_name = 'tests/for_testing.csv'
+    test_file_name = '../tests/for_testing.csv'
     Item.instantiate_from_csv(test_file_name)
     return Item.all
+
+
 def test_instantiate_from_csv(items_from_csv):
     """Проверяем метод создания экземпляров из файла. Для тестирования создан файл:"""
     # test_file_name = 'tests/for_testing.csv'
